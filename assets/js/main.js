a92bc594cb0aac56de541c421f5edd324232202d
Elsa Malle <elsamalle@gmail.com>
2026-02-10 17:21:05 -0300

    Se añaden comentarios a codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 console.log('Estamos conectados al Javascript');
 
+/** Función para cambiar el fondo de la página */
 let colorActivo = true;
-
 function cambiarFondo() {
     if (colorActivo) {
         document.body.style.backgroundColor = '#558b6e';
@@ -11,13 +11,14 @@ function cambiarFondo() {
         colorActivo = true;
     }
 };
-
+/** Función para agrandar el tamaño del texto */
 function resalta() {
     let texto = document.getElementById('nombre');
     texto.style.fontSize = "64px";
 }
-
+/** Función para achicar el tamaño del texto */
 function achica() {
     let texto = document.getElementById('nombre');
     texto.style.fontSize = "36px";
 }
+
